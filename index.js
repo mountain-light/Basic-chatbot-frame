2622fcf9d29dcd09e2b61d0a7b8fb5a3aa27864a
mountain-light <32393258+mountain-light@users.noreply.github.com>
2018-04-27 15:29:10 +0100

    introduced customer events and simplified code

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 var fs = require('fs');
-    var readline = require('readline');
-    const chalk = require('chalk');
-    var rl = readline.createInterface(process.stdin, process.stdout);
+var readline = require('readline');
+const chalk = require('chalk');
+var rl = readline.createInterface(process.stdin, process.stdout);
+var Person = require('./bot');
 
-// read box.json
-    var fetchWords = (lastWord) => {
-        fs.readFile('./jsonFiles/box.json', function(err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            data = JSON.parse(data);
-            var answer = data[lastWord];
-                if(answer)
-                    receivedReply(answer);
-                if(!answer || answer === undefined){
-                    console.log(answer = `\n          HAL > Cannot give an answer! - +
-                        Please Add Question/Reply to database`);
-                    addToDB();
-                }                      
-        });                
-    }
+ var fetchWords = (question, lastWord) => {
+    fs.readFile('./jsonFiles/box.json', (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        data = JSON.parse(data);
+        var answer = data[lastWord];
+            if(answer)
+                bot.emit('speak', answer);
+            if(!answer){
+                longSentence(question)
+            }                      
+    });          
+}
 
-// Introduction
+
+
 function intro(){
-
     console.log(`\n          'Go'  To QUESTION the KNOWLEDGE BASE`);
     console.log(`\n          'Add'  to write data to KNOWLEDGE BASE`); // not used
     console.log(`\n          'help'  to see the rules.`); // Not used
     console.log(`\n          'Exit'  will 'END' the session\n`);
 
-rl.question("Please select:> ", function(answer){
-    answer = answer.trim().toLowerCase();
-        if (answer === 'go'){                      
+    rl.question("Please select:> ", function(answer){
+        answer = answer.trim().toLowerCase();
+        if (answer === 'go'){ 
+            var clear = require('clear');clear(); // Clear the screen
+            console.log(`          Your question please\n`);                     
             start(1);        
         }
             if (answer === 'add'){
                 addData();
             }
-        if (answer === 'exit'){     
-            finish();S
-        }
-});
+            if (answer === 'exit'){     
+                finish();
+            }
+    });
 }
 
 function start(n){
-    for (var i=100; i > n; n++){
+    
+    for (var i=50; i > n; n++){
         rl.question(chalk.blue`\n          HUMAN > `, function(quest){
             var question = quest.trim().toLowerCase();
             if (question == 'exit'){
@@ -57,10 +58,6 @@ function start(n){
 }
 
 function siftQuestion(question){
-    if(question ==='undefined') {
-        console.log('Word not known - Add it to database');
-        start(1);
-    }
         var str = question.toLowerCase();
         var arr = str.split(' '); // array made
         val1 = arr.shift(); // first word from the front
@@ -73,72 +70,58 @@ function siftQuestion(question){
         val == 'who are' || val == 'what are' || val == 'why are' || val == 'when are' || val == 'where are' || val == 'how are' ||
             val == 'are you' || val == 'am i' || val == 'i am' || val == 'he is' || val == 'she is' || val == 'they are' || val == 'are they' ||
                 val == 'we are' || val == 'are we' || val == 'is it' || val == 'it is' || val=='do you') {
-        fetchWords(lastWord);
-   
-
-
-    }else{ // when the 'lastWord' does not qualify as above
+                    fetchWords(question, lastWord);
+    }else{
         longSentence(question);            
-        }
+    }
 }
-// Next job connect it to box.json
+
 function longSentence(question){
     var longQuestion = fs.readFileSync('./jsonFiles/fullSentence.json');
     var data = JSON.parse(longQuestion);
-    var words = data;
     var answer = data[question]; 
         if(answer){
-            receivedReply(answer);
-        }else if(!answer){
-            console.log(answer = `\n          BOT > Cannot give an answer!`);
-            addToDB();
-        }
-    
+            bot.emit('speak', answer);
+           start(1);
+        }else{            
+            addToDB(question);
+        }    
 }
-// It is working but it is disjointed
-function addToDB(){
-    var clear = require('clear');
-    clear();
-    console.log('Enter Question then add the Reply - <exit to END>');
-    var longQuestion = fs.readFileSync('./jsonFiles/fullSentence.json');
-    var data = JSON.parse(longQuestion);
-    var words = data;
-    rl.question(chalk.yellow`          Please insert the question > `, function(title){
-        if(title =='exit'){
-            finish();
-        }
-        var word = title.trim().toLowerCase();    
-        console.log(chalk.yellow`          Please supply the answer?\n`);
+
+function addToDB(question){
+    var clear = require('clear'); clear();
+        console.log(answer = chalk.blue `\n          BOT > Cannot give an answer to ${question}`);
+        var longQuestion = fs.readFileSync('./jsonFiles/fullSentence.json');
+        var data = JSON.parse(longQuestion);
+        var words = data;
+        var word = question.trim().toLowerCase();    
+        console.log(chalk.yellow`          Please enter your answer\n`);
     rl.question(chalk.yellow`          answer > `, function(body){
-        if(body =='exit'){
+        if(body ==='exit'){
             finish();
         }
-        var val = body;
-        words[word] = val;
-        var data = JSON.stringify(words, null, 2);    
-        fs.writeFile('./jsonFiles/fullSentence.json', data, fim);
+            var val = body;
+            words[word] = val;
+            var data = JSON.stringify(words, null, 2);    
+                fs.writeFile('./jsonFiles/fullSentence.json', data, fim);    
 
-    function fim(){    
-        console.log(`          new entry in the database`);
+function fim(){    
+    console.log(`\n          new entry in the database`);
         start(1);
-    }    
-    });   
-    });    
-}
-
-function receivedReply( answer) {  
-            console.log(chalk.green(`          BOT > ${answer} \n`));
-            start(1);
-            if (answer === 'exit'){
-                finish();
-            }
+    }
+    });      
 }
 
 function finish(){
-    var clear = require('clear');
-    clear();
+    var clear = require('clear');clear();
     console.log(`\n                       ------- ENDING PROGRAM -------`);
     process.exit();
 }
+
+    var bot = new Person('ChatBot');
+bot.on('speak', function(answer){
+    console.log(chalk.green(`          BOT > ${answer} \n`));
+    start(1);
+});
 
 intro();
